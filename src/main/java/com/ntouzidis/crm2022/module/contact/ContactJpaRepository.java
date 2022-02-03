d761917d620b42e9c630e53c95228caed856d092
Thanos Ntouzidis <thanosntouzidis@gmail.com>
2022-02-03 02:54:57 +0200

    Search contacts by company

--- src/main/java/com/ntouzidis/crm2022/module/contact/ContactJpaRepository.java
+++ src/main/java/com/ntouzidis/crm2022/module/contact/ContactJpaRepository.java
@@ -8,4 +8,6 @@ import java.util.List;
 public interface ContactJpaRepository extends JpaRepository<ContactEntity, Long> {
 
   List<ContactEntity> findAllByBusinessType(BusinessType businessType);
+
+  List<ContactEntity> findByCompanyStartsWith(String company);
 }
